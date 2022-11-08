dad5e1055273045d4b2f24713c26c7861e826cc1
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-08 20:33:40 +0200

    added connection to pool in server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,27 @@
 import express from "express";
 import app from "./app";
 import { SERVER_URI } from "./util/secrets";
+import pool from "./pool";
 
 const serverUrl = SERVER_URI;
 
-app.listen(app.get("port"), () => {});
+pool
+  .connect({
+    database: "",
+    host: "localhost",
+    user: "johnny",
+    port: 5432,
+  })
+  .then(async () => {
+    const serverStartMsg = "Express server started on port: ",
+      port = process.env.PORT;
+  });
+
+app.listen(app.get("port"), () => {
+  console.log(
+    "App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  console.log(" Press CRTL-C to stop\n");
+});
