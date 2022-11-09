89cb03a7fe3670ebb2549f904cf2fb4a2d89f091
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-09 18:43:53 +0200

    added users fixtures in test folder, and createuser in service tests

--- api/tests/ServicesTests/UsersService.test.ts
+++ api/tests/ServicesTests/UsersService.test.ts
@@ -1,6 +1,7 @@
 import pool from "../../src/pool";
 import jest from "jest";
 import { describe, expect, test } from "@jest/globals";
+import { User } from "../../src/types/UserType";
 
 beforeAll(async () => {
   await pool.connect({
@@ -13,3 +14,10 @@ beforeAll(async () => {
 afterAll(async () => {
   await pool.close();
 });
+
+const createOne = async (user: User) => {
+  await pool.query(
+    "insert into users (firstname, lastname, email, weight, age) values($1, $2, $3, $4, $5)",
+    [user.firstname, user.lastname, user.email, user.weight, user.age]
+  );
+};
---
+++ api/tests/fixtures/users.ts
@@ -0,0 +1,17 @@
+import { User } from "../../src/types/UserType";
+
+export const user1: User = {
+  firstname: "johnny",
+  lastname: "nylund",
+  email: "johnny@gmail.com",
+  age: 54,
+  weight: 84,
+};
+
+export const user2: User = {
+  firstname: "mike",
+  lastname: "bowie",
+  email: "mikey@gmail.com",
+  age: 34,
+  weight: 64,
+};
