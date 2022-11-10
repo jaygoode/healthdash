a729959f4ce9cd315fe26f57261424ee7525b7cc
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-10 22:30:43 +0200

    added crud Note services

---
+++ api/src/services/NoteServices.ts
@@ -0,0 +1,58 @@
+import { Note } from "../entities/noteEntity";
+import { AppDataSource } from "../data-source";
+
+const noteRepository = AppDataSource.getRepository(Note);
+
+const getAllNotes = async () => {
+  try {
+    const users = await noteRepository.find();
+    return users;
+  } catch (error: any) {
+    console.log(error);
+    return error.message;
+  }
+};
+
+const createOne = async (note: Note) => {
+  try {
+    return await noteRepository.save(note);
+  } catch (error: any) {
+    return error.message;
+  }
+};
+
+const getnoteById = async (id: number) => {
+  try {
+    return await noteRepository.findOneBy({ id: id });
+  } catch (error: any) {
+    return error.message;
+  }
+};
+
+const updateOne = async (id: number, update: Partial<Note>) => {
+  const foundOne = await noteRepository.findOneBy({ id: id });
+  if (foundOne) {
+    return await noteRepository.update({ id: id }, update);
+  } else {
+    return;
+  }
+};
+
+const deleteOne = async (id: number) => {
+  const foundOne = await noteRepository.findOneBy({ id: id });
+  if (foundOne) {
+    return await noteRepository.delete({ id: id });
+  } else {
+    return;
+  }
+};
+
+//.querybuilder() for more complex methods, joins and stuff
+
+export default {
+  getAllNotes,
+  createOne,
+  getnoteById,
+  deleteOne,
+  updateOne,
+};
