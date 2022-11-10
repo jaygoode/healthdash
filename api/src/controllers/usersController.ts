e505de14c18a574c126e86b1472588db33d5fe48
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-10 18:05:37 +0200

    added complete crud ops to users route

--- api/src/controllers/usersController.ts
+++ api/src/controllers/usersController.ts
@@ -11,6 +11,39 @@ const getAll = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await UserServices.createOne(req.body);
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+};
+
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await UserServices.updateOne(req.body.id, req.body);
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+};
+
+const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await UserServices.deleteOne(req.body.id);
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    return next(error);
+  }
+};
+
 export default {
   getAll,
+  createUser,
+  updateUser,
+  deleteUser,
 };
--- api/src/routers/usersRouter.ts
+++ api/src/routers/usersRouter.ts
@@ -4,5 +4,8 @@ import usersController from "../controllers/usersController";
 const usersRouter = Router();
 
 usersRouter.get("/", usersController.getAll);
+usersRouter.post("/", usersController.createUser);
+usersRouter.patch("/", usersController.updateUser);
+usersRouter.delete("/", usersController.deleteUser);
 
 export default usersRouter;
