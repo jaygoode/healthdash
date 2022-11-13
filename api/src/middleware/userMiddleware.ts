e72c1f5d6f1ea84c1c0079dd512e6395666071ec
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-13 15:01:11 +0200

    added usermiddleware

---
+++ api/src/middleware/userMiddleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { User } from "../entities/userEntity";
+import { AppDataSource } from "../data-source";
+
+const userRepository = AppDataSource.getRepository(User);
+
+export const verifyCredentials = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+  const foundUser = await userRepository.findOne({ email: email });
+  // if (foundUser) {
+  //   const checkPassword = await foundUser.comparePassword(password)
+  //   if (checkPassword) {
+  //     next()
+  //   } else {
+  //     res.status(401).send('Incorrect credentials.')
+  //   }
+  // } else {
+  //   res.status(401).send('Incorrect credentials.')
+  // }
+};
