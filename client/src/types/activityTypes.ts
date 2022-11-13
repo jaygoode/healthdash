c0ecd49ea1bed66523eb0a2668c454928b98a254
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-13 23:55:20 +0200

    added activityType.ts in client

---
+++ client/src/types/activityTypes.ts
@@ -0,0 +1,10 @@
+export interface Activity {
+  id?: number;
+  startTime: string;
+  endTime: string;
+  duration: string;
+  type: string;
+  intensity: string;
+  noteId: number;
+  userId: number;
+}
