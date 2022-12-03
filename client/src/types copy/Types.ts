00742fb91bf0fc844851b1f033ef7e013e99555b
johnny <john_newcreek@proton.me>
2022-12-03 14:55:20 +0200

    added types to client side

---
+++ client/src/types copy/Types.ts
@@ -0,0 +1,32 @@
+export interface User {
+  id: number;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  weight: number;
+  age: number;
+  username: string;
+  role: "customer" | "admin";
+  noteId?: number;
+  activityId?: number;
+}
+
+export interface Note {
+  id?: number;
+  content: string;
+  tags: string[];
+  dateCreated: Date;
+  userId: number;
+  activityId: number;
+}
+
+export interface Activity {
+  id: number;
+  createTime: Date;
+  endTime: string;
+  type: string;
+  intensity: string;
+  noteId: number;
+  userId: number;
+}
