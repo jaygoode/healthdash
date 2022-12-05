6765bf8824088418bae3ae22ba63a14fe9b5a948
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-12-05 23:35:56 +0200

    fixed tsconfig, package.json

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.scss";
 import App from "./App";
-import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
