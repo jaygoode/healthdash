922cccd143d9433fda5a8721bac9b26a3814e364
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-12-06 21:27:23 +0200

    added infoContainer and InfoSquare components and hooked them up

---
+++ client/src/components/InfoContainer.tsx
@@ -0,0 +1,14 @@
+import InfoSquare from "./InfoSquare";
+
+const InfoContainer = () => {
+  return (
+    <div className="info-container">
+      <InfoSquare />
+      <InfoSquare />
+      <InfoSquare />
+      <InfoSquare />
+    </div>
+  );
+};
+
+export default InfoContainer;
