62e0481249696a4c0afae9faf0672a485fcbafd4
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-12-10 16:33:49 +0200

    added sass and settings button in nav

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [settingsModule, setSettingsModule] = useState(false);
+  console.log(settingsModule);
   return (
     <div>
       <ul>
         <Link to="/">Home</Link>
         <Link to="/overview">Overview</Link>
+        <li>
+          <button onClick={() => setSettingsModule(false)}>Settings</button>
+        </li>
       </ul>
     </div>
   );
