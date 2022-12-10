c4bca9537b528f7689f558d0194c348c53ed77bd
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-12-10 16:36:36 +0200

    fixed button toggle in nav

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,14 +3,20 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [settingsModule, setSettingsModule] = useState(false);
-  console.log(settingsModule);
+
   return (
     <div>
       <ul>
         <Link to="/">Home</Link>
         <Link to="/overview">Overview</Link>
         <li>
-          <button onClick={() => setSettingsModule(false)}>Settings</button>
+          <button
+            onClick={() => {
+              setSettingsModule(!settingsModule);
+            }}
+          >
+            Settings
+          </button>
         </li>
       </ul>
     </div>
