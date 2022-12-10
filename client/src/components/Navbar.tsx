bdd18c778baf021d43d070ccf38a0329e3917665
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-12-10 19:26:32 +0200

    added mobilefriendly nav, functionality works, styling in progress.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,27 +3,42 @@ import { useState } from "react";
 
 const Navbar = () => {
   const [settingsModule, setSettingsModule] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
 
   return (
-    <div>
-      <ul className="nav-list">
+    <nav className="nav-list">
+      <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
+      <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={() => closeMenu()}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/overview">Overview</Link>
+          <Link to="/overview" onClick={() => closeMenu()}>
+            Overview
+          </Link>
         </li>
         <li>
           <button
             onClick={() => {
               setSettingsModule(!settingsModule);
+              closeMenu();
             }}
           >
             Settings
           </button>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 };
 
