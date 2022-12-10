55e2d2a0d9e85eb29903aee9a3c5ebf67b99c332
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-12-10 17:10:06 +0200

    added sass mixins and imported stylesheets

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import "../styles/components/Nav.scss";
 
 const Navbar = () => {
   const [settingsModule, setSettingsModule] = useState(false);
 
   return (
     <div>
-      <ul className="Nav">
+      <ul className="nav">
         <Link to="/">Home</Link>
         <Link to="/overview">Overview</Link>
         <li>
